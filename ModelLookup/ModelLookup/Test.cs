c746d2ddfbd835e9a1c7574fe9a2fa8e592a1fce
Chris Chen <cchen@futuredial.com>
2019-12-26 22:39:10 -0800

    Add samsung data.

--- ModelLookup/ModelLookup/Test.cs
+++ ModelLookup/ModelLookup/Test.cs
@@ -5,6 +5,7 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ModelLookup
@@ -41,7 +42,100 @@ namespace ModelLookup
         }
         static void samsung_test1()
         {
+            var jss = new System.Web.Script.Serialization.JavaScriptSerializer();
+            string str;
+            Regex reg = new Regex(@"SM-G970");
+            string target_ret_maker = "Samsung";
+            string target_ret_model = "GalaxyS10e";
 
+            List<Dictionary<string, object>> target_model = new List<Dictionary<string, object>>();
+            // first step: all target model to a list.
+            if (true)
+            {
+                str = System.IO.File.ReadAllText(@"data\samsung_fromdb.json");
+                ArrayList al = jss.Deserialize<ArrayList>(str);
+                foreach (Dictionary<string, object> r in al)
+                {
+                    if (r.ContainsKey("model"))
+                    {
+                        if (reg.IsMatch(r["model"].ToString()))
+                        {
+                            target_model.Add(r);
+                        }
+                    }
+                }
+                str = jss.Serialize(target_model);
+                //System.IO.File.WriteAllText("test.json", str);
+            }
+
+            // second, group by tac number
+            Dictionary<string, object> target_model_bytac = new Dictionary<string, object>();
+            foreach(Dictionary<string,object> r in target_model)
+            {
+                if (r.ContainsKey("tac"))
+                {
+                    str = r["tac"].ToString();
+                    if (target_model_bytac.ContainsKey(str))
+                    {
+                        ArrayList a = (ArrayList)target_model_bytac[str];
+                        a.Add(r);
+                    }
+                    else
+                    {
+                        ArrayList a = new ArrayList();
+                        a.Add(r);
+                        target_model_bytac.Add(str, a);
+                    }
+                }
+            }
+            //System.IO.File.WriteAllText("samsung_G930_bytac.json", jss.Serialize(target_model_bytac));
+
+            // third, count number by tac
+            Dictionary<string, object> all_samsung = new Dictionary<string, object>();
+            str = System.IO.File.ReadAllText(@"data\samsung_bytac.json");
+            all_samsung = jss.Deserialize<Dictionary<string, object>>(str);
+            Dictionary<string, int[]> target_summary = new Dictionary<string, int[]>();
+            foreach(string tac in target_model_bytac.Keys)
+            {
+                int[] summary = new int[3];
+                target_summary.Add(tac, summary);
+                if (all_samsung.ContainsKey(tac))
+                {
+                    ArrayList al = (ArrayList)all_samsung[tac];
+                    foreach(Dictionary<string,object> r in al)
+                    {
+                        int cnt = r.ContainsKey("cnt") ? (int)r["cnt"] : 0;
+                        summary[0] += cnt;
+                        if (r.ContainsKey("model"))
+                        {
+                            if (reg.IsMatch(r["model"].ToString()))
+                            {
+                                summary[1] += cnt;
+                            }
+                            else
+                            {
+                                summary[2] += cnt;
+                            }
+                        }
+                    }
+                }
+            }
+            //System.IO.File.WriteAllText("test.json", jss.Serialize(ret));
+            double th = 90.0;
+            target_model.Clear();
+            foreach(KeyValuePair<string,int[]> kvp in target_summary)
+            {
+                double d = 100.0 * kvp.Value[1] / kvp.Value[0];
+                if(d>=th)
+                {
+                    Dictionary<string, object> dic = new Dictionary<string, object>();
+                    dic.Add("uuid", kvp.Key);
+                    dic.Add("maker", target_ret_maker);
+                    dic.Add("model", target_ret_model);
+                    target_model.Add(dic);
+                }
+            }
+            System.IO.File.WriteAllText($"{target_ret_maker}_{target_ret_model}.json", jss.Serialize(target_model));
         }
         static void upload_data(Dictionary<string,object>[] records)
         {
