4de980a5d58c7926aec912aa245f2a93343c3076
cchen <chencen2000@gmail.com>
2020-01-21 15:28:32 -0800

    avia setup

---
+++ AviaSetup/AviaSetup/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AviaSetup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += delegate 
+            {
+                load_data();
+            };
+        }
+        void load_data()
+        {
+            FocusManager.SetFocusedElement(tbSerialNo.Parent, tbSerialNo);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // OK clicked
+            string sn = tbSerialNo.Text;
+            try
+            {
+                Guid g = Guid.Parse(sn);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Serial no incorrect.");
+                return;
+            }
+
+            // serail no format is ok.
+
+        }
+    }
+}
---
+++ AviaSetup/ClassLibraryHelper/Installer1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassLibraryHelper
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+        }
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+        }
+        protected override void OnCommitted(IDictionary savedState)
+        {
+            base.OnCommitted(savedState);
+        }
+        protected override void OnCommitting(IDictionary savedState)
+        {
+            MessageBox.Show("test");
+            base.OnCommitting(savedState);
+        }
+    }
+}
